9a48f0f6c2e6bcb229844ad48e261e72526fb198
Humberto-Roman <111900184+Humberto-Roman@users.noreply.github.com>
2022-11-27 20:39:28 -0600

    Add files via upload

---
+++ Trello (BD)/Trello en una tarde.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int numl{0};
+string user, passw, tabact[100][2];
+
+void LoadUsers()
+{
+	
+}
+void VerUser()
+{
+	
+}
+void DataLoad()
+{
+	for(int i=1; i < 100; i++)
+	{
+		
+	}
+}
+int main()
+{	LoadUsers()
+	cout<<"Bienvenido a un clon mas de trello \n\n";
+	cout<<"Ingrese su usuario: "; cin >> user; cout<< "\n";
+	cout<<"Ingrese la contraseña: "; cin >> passw; cout<< "\n";
+	VerUser();
+	DataLoad();
+	return 0;
+}
